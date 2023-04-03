625696395b23f57cb3720a438a868a4ce16b3fc0
karuna <sharmakaruna1610@gmail.com>
2023-04-03 07:34:03 +0530

    test2 commit

--- src/main/java/org/example/abstractclass/GitTest2.java
+++ src/main/java/org/example/abstractclass/GitTest2.java
@@ -2,6 +2,6 @@ package org.example.abstractclass;
 
 public class GitTest2 {
     public static void main(String[] args) {
-        System.out.println("I am remote changes");
+        System.out.println("I am remote changes 2");
     }
 }

c1f357977d99afb90dd38ecc9afbee78d6cf16d4
sd0804 <124291702+sd0804@users.noreply.github.com>
2023-04-03 07:47:05 +0530

    test6

--- src/main/java/org/example/abstractclass/GitTest2.java
+++ src/main/java/org/example/abstractclass/GitTest2.java
@@ -4,7 +4,7 @@ public class GitTest2 {
     public static void main(String[] args) {
 
         System.out.println("I am remote changes 3");
-         System.out.println("I am remote changes 5");
+         System.out.println("I am remote changes 6");
 
     }
 }
